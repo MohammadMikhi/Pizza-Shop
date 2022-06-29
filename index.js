2ba6fb6693b2e195176710af80ea490f1d6c94f2
Mohammad Mikhi <mohammad012010@hotmail.com>
2022-06-29 22:38:20 +0300

    code enchancement

--- index.js
+++ index.js
@@ -1,63 +1,77 @@
 function addPizza(price, pizzaType) {
     var amount = document.getElementById("amount");
     var total = document.getElementById("total");
+    var modalTotal = document.getElementById("modalTotal");
+    var classicAmount = document.getElementById('amount1')
+        , margheritaAmount = document.getElementById('amount2')
+        , pestoAmount = document.getElementById('amount3');
+    var classicName = document.getElementById('pizza1'),
+        margheritaName = document.getElementById('pizza2'),
+        pestoName = document.getElementById('pizza3');
     amount.innerHTML = parseFloat(parseFloat(amount.innerHTML) + price).toFixed(2);
     total.innerHTML = parseInt(total.innerHTML) + 1;
     if (pizzaType == 'Classic') {
-        document.getElementById("pizza1").className = "cart-modal-item d-flex align-items-center";
-        document.getElementById("amount1").value = parseInt(document.getElementById("amount1").value) + 1;
+        classicName.className = "cart-modal-item d-flex align-items-center";
+        classicAmount.value = parseInt(classicAmount.value) + 1;
     }
     if (pizzaType == 'Margherita') {
-        document.getElementById("pizza2").className = "cart-modal-item d-flex align-items-center";
-        document.getElementById("amount2").value = parseInt(document.getElementById("amount2").value) + 1;
+        margheritaName.className = "cart-modal-item d-flex align-items-center";
+        margheritaAmount.value = parseInt(margheritaAmount.value) + 1;
     }
     if (pizzaType == 'Pesto') {
-        document.getElementById("pizza3").className = "cart-modal-item d-flex align-items-center";
-        document.getElementById("amount3").value = parseInt(document.getElementById("amount3").value) + 1;
+        pestoName.className = "cart-modal-item d-flex align-items-center";
+        pestoAmount.value = parseInt(pestoAmount.value) + 1;
     }
-    document.getElementById("modalTotal").innerHTML = "$" + document.getElementById("amount").innerHTML;
+    modalTotal.innerHTML = "$" + amount.innerHTML;
 }
 function buttonMinus(price, pizzaType) {
     var amount = document.getElementById("amount");
     var total = document.getElementById("total");
+    var modalTotal = document.getElementById("modalTotal");
+    var classicAmount = document.getElementById('amount1')
+        , margheritaAmount = document.getElementById('amount2')
+        , pestoAmount = document.getElementById('amount3');
+    var classicName = document.getElementById('pizza1'),
+        margheritaName = document.getElementById('pizza2'),
+        pestoName = document.getElementById('pizza3');
     if (pizzaType == 'Classic') {
-        if (parseInt(document.getElementById('amount1').value) > 1) {
+        if (parseInt(classicAmount.value) > 1) {
             amount.innerHTML = parseFloat(parseFloat(amount.innerHTML) - price).toFixed(2);
             total.innerHTML = parseInt(total.innerHTML) - 1;
-            document.getElementById("amount1").value = parseInt(document.getElementById("amount1").value) - 1;
+            classicAmount.value = parseInt(classicAmount.value) - 1;
         }
         else {
             amount.innerHTML = parseFloat(parseFloat(amount.innerHTML) - price).toFixed(2);
             total.innerHTML = parseInt(total.innerHTML) - 1;
-            document.getElementById("amount1").value = parseInt(document.getElementById("amount1").value) - 1;
-            document.getElementById("pizza1").className = "cart-modal-item d-flex align-items-center visually-hidden";
+            classicAmount.value = parseInt(classicAmount.value) - 1;
+            classicName.className = "cart-modal-item d-flex align-items-center visually-hidden";
         }
     }
     if (pizzaType == 'Margherita') {
         if (parseInt(document.getElementById('amount2').value) > 1) {
             amount.innerHTML = parseFloat(parseFloat(amount.innerHTML) - price).toFixed(2);
             total.innerHTML = parseInt(total.innerHTML) - 1;
-            document.getElementById("amount2").value = parseInt(document.getElementById("amount2").value) - 1;
+            margheritaAmount.value = parseInt(margheritaAmount.value) - 1;
         }
         else {
             amount.innerHTML = parseFloat(parseFloat(amount.innerHTML) - price).toFixed(2);
             total.innerHTML = parseInt(total.innerHTML) - 1;
-            document.getElementById("amount2").value = parseInt(document.getElementById("amount2").value) - 1;
-            document.getElementById("pizza2").className = "cart-modal-item d-flex align-items-center visually-hidden";
+            margheritaAmount.value = parseInt(margheritaAmount.value) - 1;
+            margheritaName.className = "cart-modal-item d-flex align-items-center visually-hidden";
         }
     }
     if (pizzaType == 'Pesto') {
         if (parseInt(document.getElementById('amount3').value) > 1) {
             amount.innerHTML = parseFloat(parseFloat(amount.innerHTML) - price).toFixed(2);
             total.innerHTML = parseInt(total.innerHTML) - 1;
-            document.getElementById("amount3").value = parseInt(document.getElementById("amount3").value) - 1;
+            pestoAmount.value = parseInt(pestoAmount.value) - 1;
         }
         else {
             amount.innerHTML = parseFloat(parseFloat(amount.innerHTML) - price).toFixed(2);
             total.innerHTML = parseInt(total.innerHTML) - 1;
-            document.getElementById("amount3").value = parseInt(document.getElementById("amount3").value) - 1;
-            document.getElementById("pizza3").className = "cart-modal-item d-flex align-items-center visually-hidden";
+            pestoAmount.value = parseInt(pestoAmount.value) - 1;
+            pestoName.className = "cart-modal-item d-flex align-items-center visually-hidden";
         }
     }
-    document.getElementById("modalTotal").innerHTML = "$" + document.getElementById("amount").innerHTML;
+    modalTotal.innerHTML = "$" + amount.innerHTML;
 }
